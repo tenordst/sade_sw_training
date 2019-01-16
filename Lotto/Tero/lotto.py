e162e857985db97849c581c83bb32a3389336e7d
tenordst <tero.nordstrom@ridenuviz.com>
2019-01-16 15:28:12 +0200

    Korjattu bugi liittyen samaan arvoon

--- Lotto/Tero/lotto.py
+++ Lotto/Tero/lotto.py
@@ -5,10 +5,17 @@ max_arvo = 39
 lotto_numeroita = 7
 lotto_lisanumeroita = 3
 
+def arvottu_jo(rivi, uusi):
+    return uusi in rivi 
+
 def arvo_rivi(n):
     rivi = []
     for x in range(n):
-        rivi.append(random.randint(min_arvo,max_arvo))
+        while (True):
+            uusi_luku = random.randint(min_arvo,max_arvo)
+            if not (arvottu_jo(rivi, uusi_luku)):
+                rivi.append(uusi_luku)
+                break
     return rivi
 
 print (arvo_rivi(lotto_numeroita))
