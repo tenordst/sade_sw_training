2ff256c31e0ae57f8a52c2f4287d8b550910a7cb
Puttola <marko.puttonen@live.com>
2019-01-16 15:12:45 +0200

    Eka versio numeroiden arvonnasta

---
+++ Lotto/Marko/lotto.py
@@ -0,0 +1,12 @@
+import random
+min_arvo = 1
+max_arvo = 39
+lotto_numeroita = 7
+lotto_lisanumeroita = 3
+def arvo_rivi(n):
+    rivi = []
+    for x  in range (n):
+        rivi.append (random.randint(min_arvo, max_arvo))
+    return rivi
+
+print (arvo_rivi(lotto_numeroita))
