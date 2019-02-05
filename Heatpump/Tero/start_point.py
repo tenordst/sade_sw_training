6e22565d8078b5fbd07445bb0431b6ac8945cc88
tenordst <tero.nordstrom@ridenuviz.com>
2019-02-05 14:53:42 +0200

    Starting point for heatpump log creator

--- Heatpump/Tero/start_point.py
+++ Heatpump/Tero/start_point.py
@@ -5,11 +5,13 @@ import csv
 import locale
 import datetime
 
-# import numpy as np
-# import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
 # Log csv tiedostojen indeksit
 ulkolampotila_index = 3
+ulkolampotila = 'BT1'
 lammitys_meno_index = 5
 lammitys_tulo_index = 6
 kaivo_tulo_index = 7
@@ -20,11 +22,26 @@ toimintatila_index = 17
 kayttovesi_index = 18
 
 # Määrittää lämpötilan jolloin pumpun tulkitaan olevan päällä
-kaivo_meno_raja = 2
+kaivo_meno_raja = 2 # 0.2C
 lisalampo_raja = -400
-mittauksia = 1500
+pumppu_kaynnistyy = -60
+pumppu_pysahtyy = 0
+
+def draw_graphs(self, file):
+    # plot the data
+    fig = plt.figure(num=None, figsize=(16, 10), dpi=120)
+    ax1 = fig.add_subplot(1, 1, 1, title=file, xlabel="Aika")
+    ax1.plot(self.t, self.kaivo_menolammot, 'blue',self.t, self.kaivo_tulolammot, 'green',  self.t, self.tulolammot, 'yellow',  self.t, self.menolammot, 'cyan', self.t, self.ulkolammot, 'grey', self.t, self.kayttovesilammot, 'magenta', self.t, self.tavoitearvot, 'black')
+    ax1.legend(['Kaivo meno (C)','Kaivo tulo (C)','Lämmitys tulo (C)', 'Lämmitys meno (C)', 'Ulkolämpö (C)', 'Käyttövesi (C)', 'Tavoitelämpö (C)'])
+    ax2 = ax1.twinx()
+    ax2.plot(self.t,self.asteminuutit, 'r')		
+    ax2.legend(['Asteminuutit'])
+    fig.savefig(file + ".png")
+    fig.clf()
+    plt.close(fig)
 
 path = './'
+print (sys.argv)
 if len(sys.argv) > 1:
 	path = path + sys.argv[1] + '/'
 
@@ -36,4 +53,9 @@ for (dirpath, dirnames, filenames) in walk(path):
 		f.append(path + filename)
 	break
 
-print (f)
+df = pd.read_csv(f[0], sep='\t', skiprows=1)
+print (df.head())
+print (df[ulkolampotila])
+
+# matplotlibilla graafi joka näyttää ulkolämpötilan
+
