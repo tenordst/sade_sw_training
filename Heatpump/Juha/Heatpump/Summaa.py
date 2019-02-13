9ec7de440270085b34642e3432b7854acc2ab971
Korjausliike <korjausliike@gmail.com>
2019-02-13 11:05:21 +0200

    Summaaminen lisätty, kaikki about kasassa

--- Heatpump/Juha/Heatpump/Summaa.py
+++ Heatpump/Juha/Heatpump/Summaa.py
@@ -21,13 +21,13 @@ class Summaa():
         if len(self.lokiNimi) !=4: #annettu päivä
             self.output_file=self.lokiNimi[:-4]+'.csv' #.split('.')[0] +'.csv'
             filtteri='20'+self.lokiNimi[:2]+'-'+self.lokiNimi[2:4]+'-'+self.lokiNimi[4:6]
-            df1 = pd.read_csv('./csv/'+self.output_file)
+            df1 = pd.read_csv('./csv/'+self.output_file,delimiter =';')
             df=df1[df1['Date'].str.startswith(filtteri)] 
             paiva =df.iloc[1][0]         
         else: #  annettu kuukausi
             self.output_file=self.lokiNimi+'.csv' #.split('.')[0] +'.csv'
             filtteri='20'+self.lokiNimi[:2]+'-'+self.lokiNimi[2:4]
-            df1 = pd.read_csv('./csv/'+self.output_file)
+            df1 = pd.read_csv('./csv/'+self.output_file, delimiter =';')
             df=df1[df1['Date'].str.startswith(filtteri)]
             paiva =df.iloc[1][0][:7]
             
@@ -69,26 +69,69 @@ class Summaa():
         print('Pienin kaivon menolampo:\t\t' ,self.pieninkaivonMeno )    
         print('Keskiulkolampotila:\t\t\t',self.keskiulkolampotila,'\n')
     
-    def tulostaSummary(self):
-        #with open('./csv/'+'summary'+self.lokiNimi[:-4]+'.csv', "w") as f:
-        a=[['Päivä','Käynnistyksiä','Mittauksia','Pienin asteminuutti','Käynnissä kokonaisajasta (lämmitys)', 'Käynnissä kokonaisajasta (vesi)', 'Lisälämpö päällä', 'Keskimenolämpötila','Keskitulolämpötila', 'Pienin kaivon tulolämpö','Pienin kaivon menolämpö', 'Keskiulkolämpötila']]
-        b=[[self.paiva,self.kaynnistyksia,self.mittauksia,self.pieninAsteminuutti,self.kaynnissaKokoajastaLammitys,self.kaynnissaKokoajastaVesi,self.lisalampo,self.keskimenolampo,self.keskitulolampo,self.pieninkaivonTulo,self.pieninkaivonMeno,self.keskiulkolampotila]]
-        if len(self.lokiNimi)==4:filee= self.lokiNimi
-        else:filee=self.lokiNimi[:-4]
-        with open('./csv/'+'summary'+filee+'.csv','w') as resultFile:
-            wr = csv.writer(resultFile, dialect='excel')
-            wr.writerows(a)
-            wr.writerows(b)
+    def tulostaSummary(self):        
+        a=[['Paiva','Kaynnistyksia','Mittauksia','Pienin asteminuutti','Kaynnissa kokonaisajasta (lammitys)', 'Kaynnissa kokonaisajasta (vesi)'
+            ,'Lisalampo paalla', 'Keskimenolampotila','Keskitulolampotila', 'Pienin kaivon tulolampo','Pienin kaivon menolampo', 'Keskiulkolampotila']]
+        b=[[self.paiva,self.kaynnistyksia,self.mittauksia,self.pieninAsteminuutti,self.kaynnissaKokoajastaLammitys,self.kaynnissaKokoajastaVesi
+            ,self.lisalampo,self.keskimenolampo,self.keskitulolampo,self.pieninkaivonTulo,self.pieninkaivonMeno,self.keskiulkolampotila]]
+        if len(self.lokiNimi)==4:filee= self.lokiNimi[:2]
+        else:filee=self.lokiNimi[:-8]
+        outfile='./csv/'+'summary'+filee+'.csv'
+        if os.path.isfile(outfile):
+            mukana=0
+            df = pd.read_csv(outfile,delimiter =';',engine='python')
+            for index,i in df.iterrows():
+                if df.iloc[index]['Paiva']==self.paiva:
+                    mukana = 1 
+            if mukana!=1:
+                with open(outfile,'a',newline="\n") as resultFile:
+                    wr = csv.writer(resultFile, dialect='excel', delimiter =';')
+                    wr.writerows(b)
+                df = pd.read_csv(outfile,delimiter =';',engine='python')
+                df =df.sort_values(["Paiva"])
+                df.to_csv(outfile, sep=';',encoding='utf-8', index=False)
+        else:
+            with open(outfile,'w',newline="\n") as resultFile:
+                wr = csv.writer(resultFile, dialect='excel', delimiter =';')
+                wr.writerows(a)
+                wr.writerows(b)
+        self.plottaa(outfile)
+        
+    def plottaa(self,outfile):
+        df = pd.read_csv(outfile, delimiter =';')
+        p = df['Paiva']
+        k= df['Kaynnistyksia']
+        m = df['Mittauksia']
+        p=df['Pienin asteminuutti'] 
+        kkl =df['Kaynnissa kokonaisajasta (lammitys)']
+        kkv =df['Kaynnissa kokonaisajasta (vesi)']
+        l=df['Lisalampo paalla']
+        kml=df['Keskimenolampotila']
+        ktl=df['Keskitulolampotila']
+        pktl=df['Pienin kaivon tulolampo']
+        pkml=df['Pienin kaivon menolampo']
+        kul=df['Keskiulkolampotila']
 
-    def korjaa_csv_pilkut(self):  
-        if len(self.lokiNimi)==4:filee= self.lokiNimi
-        else:filee=self.lokiNimi[:-4]
-        with open('./csv/'+'summary'+filee+'.csv', newline=None) as f:
-            uusiTeksti=f.read()
-        while ',' in uusiTeksti:
-            uusiTeksti=uusiTeksti.replace(',',';')        
-        with open('./csv/'+'summary'+filee+'.csv', "w") as f:
-            f.write(uusiTeksti)
-        f.close()
+        fig,ax=plt.subplots(nrows=2,ncols=1,figsize=(20, 10), dpi=100)        
+        ax0_plotit=[pktl,pkml,kul,l]
+        for i in range(4):
+            ax[0].plot(ax0_plotit[i],label=["Pienin kaivon tulolampo","Pienin kaivon menolampo","Keskiulkolampotila","Lisalampo paalla"][i], lw=1)
 
-
+        ax[0].legend(fontsize=10, loc="upper left")
+        plt.ylabel("Asteita", fontsize=10)
+        ax[0].set_xlabel("Paivä/kk", fontsize=10)   
+        ax[0],ax[1].grid(True, which='major',color ="black",linestyle=':', linewidth=0.3)
+      
+        ax[1].plot(kkl,label="Käynnissä kokoajasta (läm)", lw=1)
+        ax[1].plot(kkv,label="Käynnissä kokoajasta (vesi)", lw=1)
+        ax[1].plot(kml,label="Keskimenolämpötila", lw=1)
+        ax[1].plot(ktl,label="Keskitulolämpötila", lw=1)
+        ax1=ax[1].twinx()
+        ax1.plot(p, label="Pienin asteminuutti", c="black",lw=1)  
+        ax[1].legend(fontsize=10, loc="upper left")
+        ax1.legend(fontsize=10, loc="upper right")
+       
+        image_nimi='./kuvat/summary'+self.lokiKuukausi[:2]+'.png'
+        plt.savefig(image_nimi, bbox_inches='tight')
+        plt.tight_layout() 
+        plt.show()
