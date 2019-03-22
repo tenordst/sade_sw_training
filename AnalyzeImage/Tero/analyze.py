395d3803ebf9572a65a8a853536952dac085f565
tenordst <tero.nordstrom@ridenuviz.com>
2019-03-22 09:51:48 +0200

    9.kerran ratkaisu

--- AnalyzeImage/Tero/analyze.py
+++ AnalyzeImage/Tero/analyze.py
@@ -8,9 +8,23 @@ if __name__ == "__main__":
    
     imageSource=open(sourceFile,'rb')
 
-    # Implement label detection using detect_lalbes method 
+    # Implement label detection using detect_labels method 
     # (see https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/rekognition.html#Rekognition.Client.detect_labels)
     # Note: This time let's use local image i.e. you can read the contents of the image using imagesource.read()
     # Target to return if there is a match and if yes, what is the confidence %
+
+    response = client.detect_labels(
+    Image={
+        'Bytes': imageSource.read(),
+    },
+    MinConfidence=70)
+
+    # print (json.dumps(response, indent=4))
+
+    for label in response['Labels']:
+        name = label['Name']
+        confidence = str(label['Confidence'])
+        print('Found label, name ' + name +
+               ' confidence ' + confidence + '%')
 
     imageSource.close()
