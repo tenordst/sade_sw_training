3ae4c55166abcb5a5bf7a6804665aa959d8bef63
tenordst <tero.nordstrom@ridenuviz.com>
2019-05-07 08:38:21 +0300

    12. kerran lopputulos

---
+++ ImageService/Tero/handler.py
@@ -0,0 +1,14 @@
+import json
+
+
+def hello(event, context):
+    print(json.dumps(event))
+
+    # Use this code if you don't use the http event with the LAMBDA-PROXY
+    # integration
+    """
+    return {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "event": event
+    }
+    """
